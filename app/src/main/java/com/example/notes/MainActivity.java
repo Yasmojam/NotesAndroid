bd8dffd856ac0e039d5896e0507707d0166f5f9f
Yasmin <yazzij97@gmail.com>
2020-10-08 17:18:59 +0100

    Search bar and buttons implemented.

--- app/src/main/java/com/example/notes/MainActivity.java
+++ app/src/main/java/com/example/notes/MainActivity.java
@@ -5,22 +5,99 @@ import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.opengl.Visibility;
 import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
     private RecyclerView recyclerView;
     // Used to load individual data sets into recycler rather than loading all at once and giving poor performance.
-    private RecyclerView.Adapter recyclerAdapter;
+    private NotesAdapter recyclerAdapter;
     private RecyclerView.LayoutManager recyclerLayoutManager;
+    ArrayList<NoteItem> notesList;
+
+    private LinearLayout searchContainer;
+    private EditText searchBar;
+    private ImageButton deleteButton;
+    private ImageButton addButton;
+    private ImageButton searchButton;
+    private Button clearButton;
+    private Boolean isDeleting = false;
+    private Boolean isSearching = false;
+
+
+    /**
+     *  Possible states:
+     *  none
+     *  deleting
+     *  adding
+     * */
+    private String appState = "none";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ArrayList<NoteItem> notesList = new ArrayList<>();
+        createNotesList();
+        buildRecyclerView();
+        setButtons();
+//        chooseDelButton = NotesAdapter.NotesViewHolder.findViewById(R.id.chooseDelButton);
+    }
+
+    public void setButtons () {
+        deleteButton = findViewById(R.id.delButton);
+        addButton = findViewById(R.id.addButton);
+        searchContainer = findViewById(R.id.searchContainer);
+        searchBar = findViewById(R.id.editSearch);
+        searchButton = findViewById(R.id.searchButton);
+        clearButton = findViewById(R.id.clearButton);
+
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                initiateDeletion();
+            }
+        });
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (searchContainer.isShown()){
+                    searchContainer.setVisibility(View.GONE);
+                    hideKeyboard(v);
+                }
+                else {
+                    searchContainer.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+        clearButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                searchBar.setText("");
+            }
+        });
+    }
+    public void createNotesList(){
+        notesList = new ArrayList<>();
         notesList.add(new NoteItem(R.drawable.ic_shopping_basket, "To buy", "Buy stuff", "#D88C9A"));
         notesList.add(new NoteItem(R.drawable.ic_assignment, "Assignment", "Do assignment", "#F2D0A9"));
         notesList.add(new NoteItem(R.drawable.ic_bookmark, "Here  Here bookmark Here bookmark v Here bookmark Here bookmark Here bookmark", "Bookmark is here", "#F1E3D3"));
@@ -33,13 +110,66 @@ public class MainActivity extends AppCompatActivity {
         notesList.add(new NoteItem(R.drawable.ic_shopping_basket, "To buy", "Buy stuff", "#8E7DBE"));
         notesList.add(new NoteItem(R.drawable.ic_assignment, "Assignment", "Do assignment", "#D88C9A"));
         notesList.add(new NoteItem(R.drawable.ic_bookmark, "Here bookmark", "Bookmark is here Bookmark is here Bookmark is here Bookmark is here Bookmark is herev Bookmark is here", "#F2D0A9"));
+    }
 
+    public void buildRecyclerView(){
         recyclerView = findViewById(R.id.recyclerView);
         recyclerView.setHasFixedSize(true); // keep size of the view the same size no matter number of items
         recyclerLayoutManager = new GridLayoutManager(this, 2);
         recyclerAdapter = new NotesAdapter(notesList);
-
         recyclerView.setLayoutManager(recyclerLayoutManager);
         recyclerView.setAdapter(recyclerAdapter);
+
+        recyclerAdapter.setOnItemClickListener(new NotesAdapter.OnItemCLickListener() {
+            @Override
+            public void onItemCLick(int position) {
+                changeItem(position, "clicked");
+            }
+
+            @Override
+            public void onDeleteClick(int position) {
+                removeItem(position);
+            }
+        });
+    }
+
+    public String getAppState() {
+        return appState;
+    }
+
+    public void setAppState(String appState) {
+        this.appState = appState;
+    }
+
+    public void removeItem(int position){
+        notesList.remove(position);
+        recyclerAdapter.notifyItemRemoved(position);
+    }
+
+    public void changeItem(int position, String text){
+        notesList.get(position).setHeading(text);
+        recyclerAdapter.notifyItemChanged(position);
+    }
+
+    public void initiateDeletion(){
+        if (!isDeleting){
+            isDeleting = true;
+
+            addButton.setVisibility(View.INVISIBLE);
+//            chooseDelButton.setVisibility(view.INVISIBLE);
+            deleteButton.setImageResource(R.drawable.ic_done);
+        }
+        else if (isDeleting){
+            isDeleting = false;
+            addButton.setVisibility(View.VISIBLE);
+
+            deleteButton.setImageResource(R.drawable.ic_delete);
+        }
+    }
+
+    /*Hides keyboard*/
+    private void hideKeyboard(View view) {
+        InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(view.getApplicationWindowToken(), 0);
     }
 }
--- app/src/main/java/com/example/notes/NotesAdapter.java
+++ app/src/main/java/com/example/notes/NotesAdapter.java
@@ -4,6 +4,8 @@ import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -13,32 +15,87 @@ import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class NotesAdapter extends RecyclerView.Adapter<NotesAdapter.NotesViewHolder> {
+public class NotesAdapter extends RecyclerView.Adapter<NotesAdapter.NotesViewHolder> implements Filterable {
+
+    // Filtered list which is shown to user
     private ArrayList<NoteItem> notesList;
+    // entire list
+    private ArrayList<NoteItem> noteListFull;
+    private OnItemCLickListener notesListener;
+
+    @Override
+    public Filter getFilter() {
+        return null;
+    }
+
+    public interface OnItemCLickListener{
+        void onItemCLick(int position);
+        void onDeleteClick(int position);
+    }
+
+    public void setOnItemClickListener(OnItemCLickListener listener) {
+        notesListener = listener;
+    }
+
     public static class NotesViewHolder extends RecyclerView.ViewHolder {
         public CardView cardView;
+        public ImageView choosingDelButton;
         public ImageView imageView;
         public TextView heading;
         public TextView body;
 
-        public NotesViewHolder(@NonNull View itemView) {
+        public NotesViewHolder(@NonNull View itemView, final OnItemCLickListener listener) {
             super(itemView);
             cardView = itemView.findViewById(R.id.cardView);
+            choosingDelButton = itemView.findViewById(R.id.chooseDelButton);
 //            imageView = itemView.findViewById(R.id.imageView);
             heading = itemView.findViewById(R.id.headingView);
             body = itemView.findViewById(R.id.bodyView);
+
+            // All items click listener
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (listener != null) {
+                        int position = getAdapterPosition();
+                        if (position != RecyclerView.NO_POSITION) {
+                            listener.onItemCLick(position);
+                        }
+                    }
+                }
+            });
+
+            // x button click listener
+            choosingDelButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (listener != null) {
+                        int position = getAdapterPosition();
+                        if (position != RecyclerView.NO_POSITION) {
+                            listener.onDeleteClick(position);
+                        }
+                    }
+                }
+            });
         }
+
+
+        public void toggleDelButton(){
+            choosingDelButton.setVisibility(View.VISIBLE);
+        }
+
     }
 
     public NotesAdapter(ArrayList<NoteItem> notesList) {
         this.notesList = notesList;
+        noteListFull = new ArrayList<>(notesList);
     }
 
     @NonNull
     @Override
     public NotesViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.note_item, parent, false);
-        NotesViewHolder nvh = new NotesViewHolder(v);
+        NotesViewHolder nvh = new NotesViewHolder(v, notesListener);
         return nvh;
     }
 
@@ -56,4 +113,6 @@ public class NotesAdapter extends RecyclerView.Adapter<NotesAdapter.NotesViewHol
     public int getItemCount() {
         return notesList.size();
     }
+
+
 }
